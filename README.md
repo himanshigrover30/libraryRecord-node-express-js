e5f05a2db33eb0be44d654639007418ec768bec3
himanshigrover30 <himmigrover850@gmail.com>
2023-03-13 02:40:50 +0530

    working on users section

---
+++ README.md
@@ -0,0 +1,56 @@
+# nodejs project
+
+server  >> storing certain book data
+        >> user registration 
+        >> Subscriber
+
+## fine system
+User = 6.03.2023 - 6.06.2023
+09.06.2023 =50*3=150/-
+
+3 Months (Basic)
+6 Months (Standard)
+12 Months (Premium)
+
+If the subscription type is standard  && if the subscription date is 06/3/2023
+=> then subscription valid till 06/09/2023
+
+within the subscription date => if we miss the renewal >>50/- per day
+subscription date is also been missded  >> and also missed the renewal >> 100 + 50/day
+
+# Routes and Endpoints
+
+## /users
+POST : CREATE A NEW USER
+GET  : GET all the user info here
+
+## /users/{id}
+GET : Get a user by id
+PUT : Update a user by their ID
+DELETE : Delete a user by their ID(Check if she/he still have any issued books) & (if there any fine to paid or not)
+
+## /users/subscription-details/{id}
+GET - Get user subscription details
+        >> Date of subscription
+        >> Valid till
+        >> Is there any fine
+
+## /books
+GET - Get all the books
+POST - Create/Add a new book
+
+## /books/{id}
+GET - Get a book by id
+PUT - Update a book by id
+
+## /books/issued
+GET - Get all issued books
+
+## /books/issued/withFine
+GET - Get all issued books with their fine
+
+https://github.com/himanshigrover30/libraryRecord-node-express-js.git
+
+## npm init
+## npm i nodemon -- save-dev
+
--- index.js
+++ index.js
@@ -1,1 +1,150 @@
-// library application
+const express= require("express"); // import express module here
+
+const {users}=require("./data/users.json");
+
+const app=express();  // initialize express with constant variable
+
+const PORT = 8081;
+
+app.use(express.json());  // define we use the application in json format
+
+app.get("/",(req,res)=>{
+    res.status(200).json({
+        messege:"server is up and running :-)"
+    })
+})
+
+/* 
+Route: /users
+Method: GET
+Description: Get all users
+Access: Public
+Parameters: None
+*/
+
+app.get("/users",(req,res)=>{
+    res.status(200).json({
+        success: true,
+        data: users,
+    })
+})
+
+/* 
+Route: /users/:id
+Method: GET
+Description: Get a user by id
+Access: Public
+Parameters: id
+*/
+app.get("/users/:id",(req,res)=>{
+    const {id}=req.params;
+    const user=users.find((each) =>each.id=== id);
+    if(!user){
+        return res.status(404).json({
+            status:false,
+            messege:"user does not exist"
+        })
+    }
+        return res.status(200).json({
+            status:true,
+            messege:"user exist",
+            data: user
+        })
+})
+
+/* 
+Route: /users
+Method: POST
+Description: Creating/Adding a new user
+Access: Public
+Parameters: None
+*/
+
+app.post("/users",(req,res)=>{
+    const {id,name,surname,email,subscriptionType,subscriptionDate}=req.body;
+    const user=users.find((each)=>each.id === id);
+    if(user){
+        return res.status(404).json({
+            status:false,
+            messege:"user with this id already exist"
+        })
+    }
+    users.push({
+        id,
+        name,
+        surname,
+        email,
+        subscriptionType,
+        subscriptionDate
+    })
+    return res.status(201).json({
+        status:true,
+        messege:"user added successfully",
+        data:users
+    })
+})
+
+/* 
+Route: /users/:id
+Method: PUT
+Description: Updating a user by their ID
+Access: Public
+Parameters: Id
+*/
+
+app.put("/users/:id", (req,res)=>{
+    const {id} =req.params;
+    const {data}= req.body;
+    const user=users.find((each)=>each.id===id);
+    if(!user){
+        return res.status(404).json({
+            status:false,
+            messege:"user with this id does not exist"
+        })
+    }
+    const updateUserData=users.map((each)=>{
+        if(each.id===id){
+            return{
+                ...each, //each element data present in json file
+                ...data  // updated data
+            };   
+        }
+        return each;
+    })
+    return res.status(200).json({
+        status:true,
+        messege:"User updated",
+        data:updateUserData,
+    })
+
+})
+
+/* 
+Route: /users/:id
+Method: DELETE
+Description: Deleting a user by their ID
+Access: Public
+Parameters: Id
+*/
+
+app.delete("/users/:id",(req,res)=>{
+    const {id}=req.params;
+    const user=users.find((each)=>each.id===id);
+    if(!user){
+        return res.status(404).json({
+            status:false,
+            messege:"User does not exist with this id"
+        })
+        return
+    }
+})
+
+app.get("*",(req,res)=>{
+    res.status(404).json({
+        messege:"This route doesn't exist"
+    })
+})
+
+app.listen(PORT,()=>{
+    console.log(`Server is running at port ${PORT}`)
+})  
